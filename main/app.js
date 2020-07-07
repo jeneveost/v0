9b3b01e92c4d2eccc53b4df6b8d9ee7a0a80d486
jeneveost <42324948+jeneveost@users.noreply.github.com>
2020-07-07 09:05:30 +0300

    test1
    test1

--- main/app.js
+++ main/app.js
@@ -2,3 +2,10 @@
 const firstEl = document.getElementById("bable");
 console.log(firstEl);
 */
+let counter = 0;
+// сколько раз нажата тап зона
+function tapZone() {
+    counter++;
+    const tap = document.getElementById("tap");
+
+}
